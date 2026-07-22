13316e85b618167c168ff1f90ce9d53552b3d9fc
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-22 19:40:28 +0530

    Time: 9 ms (51.92%), Space: 66 MB (30.07%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -6,7 +6,7 @@ var merge = function(intervals) {
     let result=[];
     intervals.sort((a,b)=>a[0]-b[0]);
     let temp=[[...intervals[0]]];
-    for(let i=0;i<intervals.length;i++){
+    for(let i=1;i<intervals.length;i++){
         if(temp[temp.length-1][1]>=intervals[i][0]&&temp[temp.length-1][1]>=intervals[i][1])continue;
         if(temp[temp.length-1][1]>=intervals[i][0]){
             temp[temp.length-1][1]=intervals[i][1];
