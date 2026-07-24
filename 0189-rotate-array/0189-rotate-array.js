e6457f10112d3f9159121e5cd6eecb06b3702399
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-24 21:52:54 +0530

    Time: 0 ms (100%), Space: 64.1 MB (75.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,10 +4,7 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    if(k>nums.length){
-        let mod=k%nums.length;
-        k=mod;
-    }
-    let subArr=nums.splice(nums.length-k,k);
-    nums.unshift(...subArr);
+    k=k%nums.length;
+    let arr=nums.splice(nums.length-k);
+    nums.unshift(...arr);
 };
