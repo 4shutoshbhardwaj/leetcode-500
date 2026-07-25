4c1f307ffc95b108fd4f670f4e680a56dfcdae1f
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-26 01:10:41 +0530

    Time: 6 ms (2.66%), Space: 55.7 MB (5.57%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let i=0;
+    let j=nums.length-1;
+    while(i<=j){
+        let mid=i+Math.floor((j-i)/2);
+        console.log(mid);
+        if(nums[i]>nums[j]){
+            if(nums[i]<=nums[mid]){
+                i=mid+1;
+            }else{
+                j=mid;
+            }
+        }else{
+            return nums[i];
+        }
+    }
+    // return false;
+};
