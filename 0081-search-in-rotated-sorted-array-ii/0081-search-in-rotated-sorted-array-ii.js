2f35f4cf3723aea8d31d787cc6401190a0852fe0
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-26 00:50:51 +0530

    Time: 1 ms (19.63%), Space: 55.4 MB (14.84%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    nums.sort((a,b)=>a-b);
+    let i=0;
+    let j=nums.length-1;
+    while(i<=j){
+        let mid=i+Math.floor((j-i)/2);
+        if(nums[mid]==target){
+            return true;
+        }
+        if(nums[i]<=nums[mid]){
+            if(nums[i]<=target&&nums[mid]>target){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }else{
+            if(nums[mid]<target&&nums[j]>=target){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+        }
+    }
+    return false;
+};
