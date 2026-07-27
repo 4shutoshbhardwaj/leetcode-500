c8d6e4dc6bc3edcfaefd3cf91b7fd48316ea5e5d
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-27 19:41:06 +0530

    Time: 92 ms (5.66%), Space: 74.3 MB (5.66%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
@@ -6,17 +6,19 @@
 var smallestDivisor = function(nums, threshold) {
     let i=0;
     let j=Math.max(...nums);
+    let ans=-1;
     while(i<=j){
         let mid=i+Math.floor((j-i)/2);
         let count=0;
         for(let num in nums){
             count+=Math.ceil(nums[num]/mid);
         }
-        if(threshold>=count){
+        if(count<=threshold){
+            ans=mid;
             j=mid-1;
         }else{
             i=mid+1;
         }
     }
-    return i;
+    return ans;
 };
