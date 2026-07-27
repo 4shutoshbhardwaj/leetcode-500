66c4d3c69d51498faeedfff69d121d5ffbbe1e49
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-27 17:01:46 +0530

    Time: 6 ms (93.93%), Space: 57.1 MB (85.71%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.js
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -6,14 +6,14 @@
 var minEatingSpeed = function(piles, h) {
     let i=1;
     let j=Math.max(...piles);
-    while(i<j){
+    while(i<=j){
         let mid=i+Math.floor((j-i)/2);
         let hours=0;
         for(let q=0;q<piles.length;q++){
             hours+=Math.ceil(piles[q]/mid);
         }
-        if(h>=hours){
-            j=mid;
+        if(hours<=h){
+            j=mid-1;
         }else{
             i=mid+1;
         }
