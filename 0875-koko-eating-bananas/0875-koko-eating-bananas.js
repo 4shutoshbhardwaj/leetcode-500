2580332020e382c61e7863c372416cb024ca144e
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-27 16:05:14 +0530

    Time: 4 ms (99.09%), Space: 58.5 MB (44.71%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let i=1;
+    let j=Math.max(...piles);
+    while(i<j){
+        let mid=i+Math.floor((j-i)/2);
+        let hours=0;
+        for(let q=0;q<piles.length;q++){
+            hours+=Math.ceil(piles[q]/mid);
+        }
+        if(hours<=h){
+            j=mid;
+        }else{
+            i=mid+1;
+        }
+    }
+    return i;
+};
