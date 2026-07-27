892da659f4f0cf55241f40969d4191eca9647c27
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-27 17:20:25 +0530

    Time: 27 ms (12.79%), Space: 66.1 MB (54.36%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} bloomDay
+ * @param {number} m
+ * @param {number} k
+ * @return {number}
+ */
+var minDays = function(bloomDay, m, k) {
+    let i=Math.min(...bloomDay);
+    let j=Math.max(...bloomDay);
+    if(bloomDay.length<(m*k))return -1;
+    while(i<j){
+        let mid=i+Math.floor((j-i)/2);
+        let bloomed=0;
+        console.log(mid);
+        let bouquets=0;
+        for(let q=0;q<bloomDay.length;q++){
+            if(mid>=bloomDay[q]){
+                bloomed+=1;
+                if(bloomed==k){
+                    bouquets++;
+                    bloomed=0;
+                }
+            }else{
+                bloomed=0;
+            }
+        }
+        if(bouquets<m){
+            i=mid+1;
+        }else{
+            j=mid;
+        }
+    }
+    return i;
+};
