36dfa2039a4c6b1ef88ca6e68755af0c67a88d2c
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-07-31 19:42:37 +0530

    Time: 262 ms (65.37%), Space: 58.5 MB (69.76%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    var i=0;
+    var j=matrix[i].length-1;
+    while(i<matrix.length&&j>-1){
+        if(matrix[i][j]==target)return true;
+        else if(matrix[i][j]>target)j--;
+        else i++;
+    }
+    return false;
+};
