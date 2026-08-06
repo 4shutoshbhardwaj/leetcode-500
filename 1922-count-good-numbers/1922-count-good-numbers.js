0fbcda2d8a4c67a32c09b67ce9fcd88a4d4fcc1b
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-07 02:40:07 +0530

    Time: 2 ms (60.66%), Space: 56.9 MB (16.39%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countGoodNumbers = function(n) {
+    let odd=Math.floor(n/2);
+    let eve=n-odd;
+    const MOD=1000000007n;
+
+    function func(num,pow){
+        pow=BigInt(pow);
+        num=BigInt(num);
+        let res=1n;
+        while(pow>0n){
+            if(pow%2n==1n){
+                res=(res*num)%MOD;
+            }
+            num=(num*num)%MOD;
+            // pow=Math.floor(pow/2);
+            pow>>=1n;
+        }
+        return res;
+    }
+
+    return Number((func(4,odd)*func(5,eve))%MOD);
+};
