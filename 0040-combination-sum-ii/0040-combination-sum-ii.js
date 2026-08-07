86db32e2b4913cb37bb0843bc157cee135c45fc1
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-08 02:13:14 +0530

    Time: 42 ms (5.62%), Space: 61.5 MB (5.32%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    let i=0;
+    let arr=[];
+    let obj={};
+    candidates.sort((a,b)=>a-b);
+    function func(i,sum,subArr){
+        if(sum==target){
+            subArr.sort((a,b)=>a-b);
+            let s=subArr.join("");
+            if(obj[s]==undefined){
+                console.log(subArr,sum);
+                arr.push([...subArr]);
+                obj[s]=1;
+            }
+            return;
+        }
+        if(sum>target)return;
+        if(i>=candidates.length)return;
+        for(let j=i;j<candidates.length;j++){
+            if(j>i&&candidates[i]==candidates[j])continue;
+            sum+=candidates[j];
+            subArr.push(candidates[j]);
+            func(j+1,sum,subArr);
+            sum-=candidates[j];
+            subArr.pop();
+        }
+    }
+    func(0,0,[]);
+    return arr;
+};
