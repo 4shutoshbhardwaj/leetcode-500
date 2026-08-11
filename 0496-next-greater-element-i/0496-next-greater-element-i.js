2c67da8c6ee952cd058f56055bc1d22ad7fdf120
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-11 05:39:09 +0530

    Time: 4 ms (51.13%), Space: 58.3 MB (23.27%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,21 +4,22 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1,nums2) {
-    let stack=[];
+    let st=[nums2[0]];
     let obj={};
-    for(let i=0;i<nums2.length;i++){
-        while(stack.length>0&&nums2[i]>stack[stack.length-1]){
-            let num=stack.pop();
-            obj[num]=nums2[i];
-        }
-        stack.push(nums2[i]);
+    for(let i=1;i<nums2.length;i++){
+        // console.log(st,obj,st[st.length-1],nums2[i]);
+            while(st[st.length-1]<nums2[i]&&st.length>0){
+                obj[st[st.length-1]]=nums2[i];
+                st.pop();
+            }
+        st.push(nums2[i]);
     }
-    while(stack.length>0){
-        obj[stack.pop()]=-1;
+    while(st.length>0){
+        obj[st[st.length-1]]=-1;
+        st.pop();
     }
-    let ans=[];
     for(let i=0;i<nums1.length;i++){
-        ans.push(obj[nums1[i]]);
+        nums1[i]=obj[nums1[i]];
     }
-    return ans;
+    return nums1;
 };
