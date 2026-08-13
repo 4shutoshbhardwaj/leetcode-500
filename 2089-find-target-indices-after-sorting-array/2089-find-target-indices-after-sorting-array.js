6c08ba9ddb20aa290211ca7b98f92136f4c4c574
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-13 18:13:37 +0530

    Time: 3 ms (31.5%), Space: 55.8 MB (58.5%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+    nums.sort((a,b)=>a-b);
+    let ans=[];
+    let t=false;
+    for(let i=0;i<nums.length;i++){
+        while(nums[i]==target){
+            ans.push(i);
+            i++;
+        }
+    }
+    return ans;
+};
