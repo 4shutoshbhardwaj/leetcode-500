515e421e9c93caf8a019ec465fdbd5332b14cff5
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-16 16:50:26 +0530

    Time: 7 ms (64.22%), Space: 55.7 MB (88.5%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -3,20 +3,21 @@
  * @param {number} goal
  * @return {number}
  */
-var numSubarraysWithSum = function(nums, goal) {
-    let obj={0:1};
-    let ans=0;
-    let sum=0;
-    for(let i=0;i<nums.length;i++){
-        sum+=nums[i];
-        if(obj[sum-goal]!=undefined){
-            ans+=obj[sum-goal];
+var numSubarraysWithSum = function(nums,goal) {
+    function func(goal){
+        if(goal<0)return 0;
+        let i=0;
+        let sum=0;
+        let count=0;
+        for(let j=0;j<nums.length;j++){
+            sum+=nums[j];
+            while(sum>goal){
+                sum-=nums[i];
+                i++;
+            }
+            count+=j-i+1;
         }
-        if(obj[sum]==undefined){
-            obj[sum]=1;
-        }else{
-            obj[sum]++;
-        }
+        return count;
     }
-    return ans;
+    return func(goal)-func(goal-1);
 };
