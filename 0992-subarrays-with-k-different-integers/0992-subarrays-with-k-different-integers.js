bfa89997d615682b6c3e64e58ddd91b3c6edd01c
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-17 18:05:23 +0530

    Time: 91 ms (7.65%), Space: 63.7 MB (55.88%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysWithKDistinct = function(nums, k) {
+    function func(k){
+        let i=0;
+        let j=0;
+        let obj={};
+        let ans=0;
+        let count=0;
+        while(j<nums.length){
+            if(obj[nums[j]]==undefined){
+                obj[nums[j]]=1;
+                count++;
+            }else{
+                obj[nums[j]]++;
+            }
+            while(count>k){
+                obj[nums[i]]--;
+                if(obj[nums[i]]==0){
+                    delete obj[nums[i]];
+                    count--;
+                }
+                i++;
+            }
+            ans+=j-i+1;
+            j++;
+        }
+        return ans;
+    }
+    return func(k)-func(k-1);
+};
