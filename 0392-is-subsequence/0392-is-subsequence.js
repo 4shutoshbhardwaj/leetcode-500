be2ff88e1c1eea9b6bfa84eaa3a98630d0d5e444
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-23 12:01:56 +0530

    Time: 1 ms (70.93%), Space: 54 MB (91.17%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let arr=s.split("");
+    let j=0;
+    for(let i=0;i<t.length;i++){
+        if(s[j]==t[i]){
+            j++;
+        }
+    }
+    if(j>=s.length)return true;
+    else return false;
+};
