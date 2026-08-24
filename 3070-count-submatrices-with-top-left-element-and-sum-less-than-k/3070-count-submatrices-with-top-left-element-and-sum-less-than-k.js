98170e18a7d1ae6cf5df063ba244501153601796
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-24 18:50:49 +0530

    Time: 37 ms (12%), Space: 84 MB (8%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.js
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.js
@@ -5,6 +5,7 @@
  */
 var countSubmatrices = function(grid, k) {
     let mat=[];
+    let count=0;
     for(let i=0;i<grid.length;i++){
         mat.push([]);
         for(let j=0;j<grid[i].length;j++){
@@ -17,11 +18,6 @@ var countSubmatrices = function(grid, k) {
             }else if(j>0&&i>0){
                 mat[i][j]=mat[i-1][j-1]+(mat[i-1][j]-mat[i-1][j-1])+(mat[i][j-1]-mat[i-1][j-1])+grid[i][j];
             }
-        }
-    }
-    let count=0;
-    for(let i=0;i<grid.length;i++){
-        for(let j=0;j<grid[i].length;j++){
             if(mat[i][j]<=k)count++;
         }
     }
