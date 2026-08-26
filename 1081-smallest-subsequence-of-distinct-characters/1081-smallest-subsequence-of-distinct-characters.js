bdbb74083a1d7b6717b5742e1ecf8dc2a2397481
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-26 09:52:36 +0530

    Time: 241 ms (6.3%), Space: 64.8 MB (5.55%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestSubsequence = function(s) {
+    let arr=[];
+    let obj={};
+    let count=0;
+    for(let i=0;i<s.length;i++){
+        if(obj[s[i]]==undefined){
+            obj[s[i]]=1;
+            count++;
+        }else{
+            obj[s[i]]++;
+        }
+    }
+    let stackObj={};
+    for(let i=0;i<s.length;i++){
+        console.log(obj,stackObj,arr,s[i])
+        if(arr[arr.length-1]>s[i]){
+            while(arr.length>0&&arr[arr.length-1]>s[i]&&obj[arr[arr.length-1]]>0&&stackObj[s[i]]==undefined){
+                let x=arr.pop();
+                stackObj[x]--;
+                if(stackObj[x]==0){
+                    delete stackObj[x];
+                }
+            }
+            if(stackObj[s[i]]==undefined){
+                stackObj[s[i]]=1;
+                arr.push(s[i]);
+            }
+            obj[s[i]]--;
+        }else{
+            if(stackObj[s[i]]==undefined){
+                stackObj[s[i]]=1;
+                arr.push(s[i]);
+            }
+            obj[s[i]]--;
+        }
+    }
+    return arr.join("");
+    // console.log(arr);
+    // console.log("a"<"b");
+};
