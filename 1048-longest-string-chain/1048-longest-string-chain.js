6087dbf94b22a9dabe9e61a4e4cc0cfb8da6e658
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-30 14:06:36 +0530

    Time: 130 ms (12.79%), Space: 78.7 MB (12.79%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var longestStrChain = function(words) {
+    words.sort((a,b)=>a.length-b.length);
+    let obj={};
+    let maxCount=1;
+    for(let i=0;i<words.length;i++){
+        let word=words[i];
+        obj[word]=1;
+        for(let j=0;j<word.length;j++){
+            let temp=word.slice(0,j)+word.slice(j+1);
+            if(obj[temp]!=undefined){
+                obj[word]=Math.max(obj[word],obj[temp]+1);
+            }
+        }
+        maxCount=Math.max(maxCount,obj[word]);
+    }
+    console.log(obj);
+    return maxCount;
+};
