0f24dbcc4ab43b2d1e7a7581f1af0f793f407e9d
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-08-30 21:16:59 +0530

    Time: 7 ms (7.74%), Space: 58.7 MB (15.7%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,34 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let prev=chars[0];
+    let count=1;
+    let s="";
+    for(let i=1;i<chars.length;i++){
+        let curr=chars[i];
+        if(curr==prev){
+            count++;
+        }else{
+            if(count>1){
+                s+=prev+count;
+            }else{
+                s+=prev;
+            }
+            count=1;
+        }
+        prev=curr
+    }
+    if(count>1){
+        s+=prev+count;
+    }else{
+        s+=prev;
+    }
+    while(chars.length>0)chars.pop();
+    for(let i=0;i<s.length;i++){
+        chars.push(s[i]);
+    }
+    console.log(chars);
+    return chars.length;
+};
