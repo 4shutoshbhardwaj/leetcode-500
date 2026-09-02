104b01e1e3093e313ecc31f09c2671a3a908fefb
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-02 21:59:47 +0530

    Time: 40 ms (27.84%), Space: 65.2 MB (25.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,15 +4,21 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
+    let j=0;
     let count=0;
-    for(let i=0;i<nums.length;i++){
-        let sum=0;
-        for(let j=i;j<nums.length;j++){
-            sum+=nums[j];
-            if(sum==k){
-                count++;
-            }
+    let sum=0;
+    let obj={0:1};
+    while(j<nums.length){
+        sum+=nums[j];
+        if(obj[sum-k]){
+            count+=obj[sum-k];
         }
+        if(obj[sum]==undefined){
+            obj[sum]=1;
+        }else{
+            obj[sum]++;
+        }
+        j++;
     }
     return count;
 };
