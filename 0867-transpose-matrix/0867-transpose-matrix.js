dd39f7662bc157b1b5a83f27f8d5a902490a4ceb
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-05 16:14:41 +0530

    Time: 0 ms (100%), Space: 57.5 MB (68.75%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+    let arr=[];
+    for(let i=0;i<matrix[0].length;i++){
+        arr.push([]);
+        for(let j=0;j<matrix.length;j++){
+            arr[i].push(matrix[j][i]);
+        }
+    }
+    return arr;
+};
