808217ca24327b43b24486b61d3ec6e3e2fdc80f
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-08 15:42:24 +0530

    Time: 16 ms (24.32%), Space: 57.3 MB (38.48%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a,b)=>a-b);
+    let min=nums[0]+nums[1]+nums[2];
+    for(let i=0;i<nums.length-2;i++){
+        let l=i+1;
+        let r=nums.length-1;
+        while(l<r){
+            let num=nums[i]+nums[l]+nums[r];
+            if(Math.abs(num-target)<Math.abs(min-target)){
+                min=num;
+            }
+            if(target<(nums[i]+nums[l]+nums[r])){
+                r--;
+            }else if(target>(nums[i]+nums[l]+nums[r])){
+                l++;
+            }else{
+                return num;
+            }
+        }
+    }
+    return min;
+};
