9d7c7ba798b99b7139e522a6747382692bea5839
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-08 08:15:07 +0530

    Time: 6 ms (72.39%), Space: 60.9 MB (55.75%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let i=0;
+    let j=s.length-1;
+    let count=0;
+    while(i<j){
+        if(s[i]==s[j]){
+            i++;
+            j--;
+        }else{
+            count++;
+            break;
+        }
+    }
+    if(i>=j)return true;
+    let a=i+1;
+    let b=j;
+    while(a<b){
+        if(s[a]==s[b]){
+            a++;
+            b--;
+        }else{
+            break;
+        }
+    }
+    if(a>=b)return true;
+    a=i;
+    b=j-1;
+    console.log(a,b);
+    while(a<b){
+        if(s[a]==s[b]){
+            a++;
+            b--;
+        }else{
+            break;
+        }
+    }
+    if(a>=b)return true;
+    return false;
+};
