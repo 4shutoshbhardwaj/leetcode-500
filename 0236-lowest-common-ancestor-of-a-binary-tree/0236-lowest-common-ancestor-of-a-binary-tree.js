5cc9e41a3a322c26b88d3ba4fdb0307fd12085d4
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-12 17:29:03 +0530

    Time: 93 ms (64.2%), Space: 90.7 MB (14.35%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,37 +12,15 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    let result=[];
-    function func(node,i,j,arr){
-        if(!node)return;
-        arr.push([i,j,node.val]);
-        if(node.val==q.val||node.val==p.val){
-            result.push([...arr]);
-        }
-        func(node.left,i-1,j+1,arr);
-        func(node.right,i+1,j+1,arr);
-        arr.pop();
+    function func(node){
+        if(!node)return null;
+        if(node.val==q.val||node.val==p.val)return node;
+        let left=func(node.left);
+        let right=func(node.right);
+        if(left==null)return right;
+        if(right==null)return left;
+        return node;
     }
-    func(root,0,0,[]);
-    let [ar1,ar2]=result;
-    let t=false;
-    for(let i=ar1.length-1;i>=0;i--){
-        for(let j=ar2.length-1;j>=0;j--){
-            if(ar1[i][0]==ar2[j][0]&&ar1[i][1]==ar2[j][1]&&ar1[i][2]==ar2[j][2]){
-                result=[ar1[i][0],ar1[i][1]];
-                t=true;
-            }
-        }
-        if(t)break;
-    }
-    function func1(node,i,j){
-        if(!node)return;
-        if(i==result[0]&&j==result[1]){
-            result=node;
-        }
-        func1(node.left,i-1,j+1);
-        func1(node.right,i+1,j+1);
-    }
-    func1(root,0,0);
-    return result;
+    console.log(func(root));
+    return func(root);
 };
