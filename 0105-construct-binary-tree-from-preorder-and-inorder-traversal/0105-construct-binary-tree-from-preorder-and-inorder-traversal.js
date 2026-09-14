129e586b7bba65323dd7902a8309c2f11b153ab2
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-15 01:55:49 +0530

    Time: 76 ms (14.22%), Space: 132.9 MB (11.26%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -15,7 +15,7 @@ var buildTree = function(preorder, inorder) {
     let primaryNode=new TreeNode(preorder[0]);
     function func(preorder,inorder,node){
         // console.log(preorder,inorder,node);
-        if((preorder.length==1&&inorder.length==1)||(preorder.length==0&&inorder.length==0))return;
+        if((preorder.length==0))return;
         let midI;
         for(let i=0;i<inorder.length;i++){
             if(node.val==inorder[i]){
@@ -30,12 +30,12 @@ var buildTree = function(preorder, inorder) {
         // console.log(inorderLeft,inorderRight,preorderLeft,preorderRight);
         if(preorderLeft.length>=1){
             node.left=new TreeNode(preorderLeft[0]);
+            func(preorderLeft,inorderLeft,node.left);
         }
         if(preorderRight.length>=1){
             node.right=new TreeNode(preorderRight[0]);
+            func(preorderRight,inorderRight,node.right);
         }
-        func(preorderLeft,inorderLeft,node.left);
-        func(preorderRight,inorderRight,node.right);
     }
     func(preorder,inorder,primaryNode);
     // console.log(primaryNode);
