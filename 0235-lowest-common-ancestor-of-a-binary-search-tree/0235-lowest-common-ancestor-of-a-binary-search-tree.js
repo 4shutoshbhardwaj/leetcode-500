79b18beb29538c670fb82324b9679f75c8712c3c
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-17 16:17:49 +0530

    Time: 76 ms (6.04%), Space: 63.5 MB (98.49%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,36 +13,14 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    let ar=[];
-    function func(node,z){
-        if(!node)return;
-        if(node.val>z.val){
-            ar.push(node);
-            func(node.left,z);
-        }else if(node.val<z.val){
-            ar.push(node);
-            func(node.right,z);
+    while(root){
+        if(root.val<q.val&&root.val<p.val){
+            root=root.right;
+        }else if(root.val>q.val&&root.val>p.val){
+            root=root.left;
         }else{
-            ar.push(node);
-            return;
+            return root;
         }
     }
-    func(root,p);
-    let ar1=[...ar];
-    ar=[];
-    func(root,q);
-    let ar2=[...ar];
-    let ans;
-    for(let i=ar1.length-1;i>=0;i--){
-        let t=false;
-        for(let j=ar2.length-1;j>=0;j--){
-            if(ar1[i].val==ar2[j].val){
-                ans=ar1[i];
-                t=true;
-                break;
-            }
-        }
-        if(t)break;
-    }
-    return ans;
+    return null;
 };
