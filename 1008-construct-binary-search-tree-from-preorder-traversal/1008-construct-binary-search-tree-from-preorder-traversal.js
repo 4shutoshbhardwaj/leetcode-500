405836b91d470962ff3a22070f31affb6d0f4158
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-18 13:03:53 +0530

    Time: 1 ms (48.76%), Space: 57 MB (43.8%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
@@ -11,33 +11,15 @@
  * @return {TreeNode}
  */
 var bstFromPreorder = function(preorder) {
-    let inorder=[...preorder];
-    inorder.sort((a,b)=>a-b);
-    let primaryNode=new TreeNode(preorder[0]);
-    function func(node,inorder,preorder){
-        if(!node)return;
-        if(preorder.length==0)return;
-        let idx;
-        for(let i=0;i<inorder.length;i++){
-            if(preorder[0]==inorder[i]){
-                idx=i;
-                break;
-            }
+    let i=0;
+    function func(max){
+        if(i==preorder.length||preorder[i]>max){
+            return null;
         }
-        let inorderLeft=inorder.slice(0,idx);
-        let inorderRight=inorder.slice(idx+1);
-        let preorderLeft=preorder.slice(1,inorderLeft.length+1);
-        let preorderRight=preorder.slice(inorderLeft.length+1);
-        if(preorderLeft.length>=1){
-            node.left=new TreeNode(preorderLeft[0]);
-            func(node.left,inorderLeft,preorderLeft);
-        }
-        if(preorderRight.length>=1){
-            node.right=new TreeNode(preorderRight[0]);
-            func(node.right,inorderRight,preorderRight);
-        }
-        // console.log(inorderLeft,inorderRight,preorderLeft,preorderRight);
+        let node=new TreeNode(preorder[i++]);
+        node.left=func(node.val);
+        node.right=func(max);
+        return node;
     }
-    func(primaryNode,inorder,preorder);
-    return primaryNode;
+    return func(Infinity);
 };
