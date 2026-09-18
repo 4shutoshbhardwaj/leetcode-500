c0119bbe6e595f8724a8969a72b6022178e10ec8
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-18 16:43:12 +0530

    Time: 11 ms (5.13%), Space: 62.5 MB (96.7%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    let obj={};
+    let t=false;
+    function func(node){
+        if(!node)return;
+        let key=k-node.val;
+        if(obj[key]){
+            t=true;
+        }else{
+            obj[node.val]=1;
+        }
+        func(node.left);
+        func(node.right);
+    }
+    func(root);
+    return t;
+};
