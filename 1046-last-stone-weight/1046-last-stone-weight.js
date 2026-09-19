593c1da97e6dc5ac8a36ae7211bb5213004faeb2
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-19 22:05:24 +0530

    Time: 9 ms (14.48%), Space: 56.7 MB (15.07%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,17 +3,29 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    let arr=[...stones];
-    arr.sort((a,b)=>a-b);
-    while(arr.length>1){
-        if(arr.length>=2){
-            let last=arr.pop();
-            let secondLast=arr.pop();
-            console.log(last,secondLast,arr);
-            arr.push((last-secondLast));
+    let arr=[];
+    function pushVal(val){
+        if(arr.length==0){
+            arr.push(val);
+        }else{
+            let temp=[];
+            while(val<arr[arr.length-1]){
+                temp.push(arr.pop());
+            }
+            arr.push(val);
+            while(temp.length>0){
+                arr.push(temp.pop());
+            }
         }
-        arr.sort((a,b)=>a-b);
     }
-    // console.log(arr);
+    while(stones.length>0){
+        pushVal(stones.pop());
+    }
+    console.log(arr);
+    while(arr.length>1){
+        let last=arr.pop();
+        let secondLast=arr.pop();
+        pushVal((last-secondLast));
+    }
     return arr[0];
 };
