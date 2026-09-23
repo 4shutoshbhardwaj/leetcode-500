22f36136db615fbd3675da8575ffa9e18e6e1e1a
Ashutosh Bhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2026-09-23 20:00:45 +0530

    Time: 2 ms (71.19%), Space: 56.5 MB (71.61%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.js
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.js
@@ -3,22 +3,22 @@
  * @return {boolean}
  */
 var isBipartite = function(graph) {
-    let adj=Array(graph.length).fill(null).map(()=>[]);
-     for(let i=0;i<graph.length;i++){
-        for(let j=0;j<graph[i].length;j++){
-            adj[i].push(graph[i][j]);
-        }
-    }
-    let visited=Array(adj.length).fill(-1);
-    for(let i=0;i<adj.length;i++){
+    // let adj=Array(graph.length).fill(null).map(()=>[]);
+    //  for(let i=0;i<graph.length;i++){
+    //     for(let j=0;j<graph[i].length;j++){
+    //         adj[i].push(graph[i][j]);
+    //     }
+    // }
+    let visited=Array(graph.length).fill(-1);
+    for(let i=0;i<graph.length;i++){
         if(visited[i]!=-1)continue;
         let q=[i];
         visited[i]=0;
         let z=0;
         while(z<q.length){
             let currNode=q[z];
-            for(let j=0;j<adj[currNode].length;j++){
-                let nextNode=adj[currNode][j];
+            for(let j=0;j<graph[currNode].length;j++){
+                let nextNode=graph[currNode][j];
                 if(visited[nextNode]==-1){
                     visited[nextNode]=1-visited[currNode];
                     q.push(nextNode);
